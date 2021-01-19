91ed24ca4a376fff054d95233d4aa2aa2c26c6f5
Muhammad Zeeshan <38476491+mzeesh2@users.noreply.github.com>
2021-01-19 15:50:54 -0600

    Bottom Navigation Added

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -1,32 +1,31 @@
 package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
-    private Button savedRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        savedRecipesButton = (Button) findViewById(R.id.savedRecipes);
-        savedRecipesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSavedRecipes();
-            }
-        });
-    }
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.savedFragment, R.id.shoppingListFragment).build();
 
-    public void openSavedRecipes() {
-        Intent intent = new Intent(this, SavedRecipes.class);
-        startActivity(intent);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
-
 }
