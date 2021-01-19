e224e9ff1e56256257131a8097fae3ef4ecfb862
Omar <omohammed7869@gmail.com>
2021-01-19 13:10:58 -0600

    Created a new SavedRecipes activity. Added button on homepage

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button savedRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        savedRecipesButton = (Button) findViewById(R.id.button);
+        savedRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSavedRecipes();
+            }
+        });
     }
+
+    public void openSavedRecipes() {
+        Intent intent = new Intent(this, SavedRecipes.class);
+        startActivity(intent);
+    }
+
 }
