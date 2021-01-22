cf4cbaecbf2e750856508c8a334d09590c853106
Omar <omohammed7869@gmail.com>
2021-01-22 14:41:40 -0600

    Added recipe item model

--- app/src/main/java/com/example/recipeapp/RecipeItem.java
+++ app/src/main/java/com/example/recipeapp/RecipeItem.java
@@ -1,4 +1,27 @@
 package com.example.recipeapp;
 
 public class RecipeItem {
+    private int mImageResource;
+    private String name;
+    private String description;
+
+    public RecipeItem(int image, String name, String description) {
+        mImageResource = image;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getImageResource() {
+        return mImageResource;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
 }
